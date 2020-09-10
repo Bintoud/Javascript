d0a3a94e16a7eb0e1b26324c55fa1107ff508f5d
Bintoud <68588582+Bintoud@users.noreply.github.com>
2020-09-10 16:58:34 +0200

    sauvegarde10/09

--- map_adventure/map1.js
+++ map_adventure/map1.js
@@ -1,2 +1,11 @@
 'use strict'
 
+var bdays = ['08-14', '10-04', '04-21'];
+
+// we want a new array where the birthdays will be in the format: MM/DD
+// the elem parameter will be each element from the original array
+var bdays2 = bdays.map(function(elem) {
+  return elem.replace('-', '/');
+});
+
+console.log(bdays2); // => ['08/14', '10/04', '04/21'] 
