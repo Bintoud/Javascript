01c20eca96662b1494a3cbafcb8c7dd1543a56e1
Bintoud <68588582+Bintoud@users.noreply.github.com>
2020-09-14 12:39:24 +0200

    sauv14/09dos

--- map_adventure/map8.js
+++ map_adventure/map8.js
@@ -16,4 +16,8 @@ var arr2 = arr.map(function (key) {
 
 console.log(arr2);
 
-
+/* ligne 13
+ le paramètre de ma fonction key contient mon array initial
+ je crée un new key qui s'appel fullname ,je lui atribu l'objet firstname et surname (concantaner)
+ je return key qui contient mon array (arr) et mon new array(fullname);
+*/
--- map_adventure/map9.js
+++ map_adventure/map9.js
@@ -4,8 +4,8 @@
 var arr = ['apple', 'croissant', 'pear',];
 
 var arr2 = arr.map(function(key) {
-    var fruit = '<li>' + key + '</li>';
-    return fruit;
-});
+    var fruit = '<li>' + key + '</li>'; //fruit contient la concaténation des string en html <li></li>
+    return fruit;                       // et de key qui contient les valeur de mon array (arr);
+});                                     // ce qui me return chaque élément de mon array + <li></li>
 
 console.log(arr2);
